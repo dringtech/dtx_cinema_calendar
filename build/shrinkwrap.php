f4f03f0b30227e8951fb16ef723a87044ed14399
Giles Dring <gilesdring@gmail.com>
2019-09-18 23:20:22 +0100

    Adding shrinkwrap script to build chain

---
+++ build/shrinkwrap.php
@@ -0,0 +1,26 @@
+<?php
+$PLUGIN_FILE = $argv[1];
+function flatten($file) {
+    $dir = dirname($file);
+    ob_start();
+    readfile(realpath($file));
+    $source_code = ob_get_contents();
+    ob_end_clean();
+    $flattened_file = preg_replace_callback(
+        '/(?:include|require)(?:_once){0,1}\s+[\'"](.*?)[\'"]\s*;/',
+        function ($m) use ($dir) {
+            $include_file = join('/', [$dir, $m[1]]);
+            $included_code = flatten($include_file);
+            return <<<INCLUDE
+// START OF REPLACED $m[0]
+?>$included_code<?php
+// END OF REPLACED $m[0]
+INCLUDE;
+        },
+        $source_code
+    );
+    return $flattened_file;        
+}
+
+echo flatten($PLUGIN_FILE);
+?>
--- src/dtx_cinema_calendar.php
+++ src/dtx_cinema_calendar.php
@@ -17,7 +17,7 @@
 // 1 = Plugin help is in raw HTML.  Not recommended.
 # $plugin['allow_html_help'] = 0;
 
-$plugin['version'] = '0.8.1';
+$plugin['version'] = '0.8.2';
 $plugin['author'] = 'Giles Dring';
 $plugin['author_uri'] = 'http://dringtech.com/';
 $plugin['description'] = 'Manage showing times for cinema';
