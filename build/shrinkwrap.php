efab0550c43c31f59e32e9c3ca30c37358bdccdc
Giles Dring <gilesdring@gmail.com>
2019-09-19 08:47:28 +0100

    Fixing flattenator

--- build/shrinkwrap.php
+++ build/shrinkwrap.php
@@ -1,24 +1,31 @@
 <?php
 $PLUGIN_FILE = $argv[1];
 function flatten($file) {
-    $dir = dirname($file);
+    $original_include_path = set_include_path(dirname($file));
     ob_start();
     readfile(realpath($file));
     $source_code = ob_get_contents();
     ob_end_clean();
     $flattened_file = preg_replace_callback(
-        '/(?:include|require)(?:_once){0,1}\s+[\'"](.*?)[\'"]\s*;/',
-        function ($m) use ($dir) {
-            $include_file = join('/', [$dir, $m[1]]);
+        '/((?:include|require)(?:_once){0,1})\s+[\'"](.*?)[\'"]\s*;/',
+        function ($m) {
+            $action = $m[1];
+            $include_file = stream_resolve_include_path($m[2]);
             $included_code = flatten($include_file);
             return <<<INCLUDE
-// START $m[0]
+/**
+ * START $m[0]
+ * $action -> $include_file
+ */
 ?>$included_code<?php
-// END $m[0]
+/**
+ * END $m[0]
+ */
 INCLUDE;
         },
         $source_code
     );
+    set_include_path($original_include_path);   
     return preg_replace('/\?>\s*<\?php/', '', $flattened_file);
 }
 
